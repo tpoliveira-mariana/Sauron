0bed3994ac830f5427b2f143b17d8b661532214b
tpoliveira-mariana <mariana.tpdeoliveira@gmail.com>
2020-04-02 20:54:57 +0100

    Input is checked on SiloFrontend.

--- eye/src/main/java/pt/tecnico/sauron/A20/eye/EyeApp.java
+++ eye/src/main/java/pt/tecnico/sauron/A20/eye/EyeApp.java
@@ -48,46 +48,44 @@ public class EyeApp {
 		}
 	}
 
-	private static void processCameraObservations(String cameraName, double lat, double lon) {
-		if (registerCamera(cameraName, lat, lon)) {
-			int[] info = {0, 0};
-			try (Scanner scanner = new Scanner(System.in)) {
-				List<List<String>> observations = new ArrayList<>();
+	private static void processCameraObservations(String cameraName,  double lat, double lon){
+		try {
+			_frontend.camJoin(cameraName, lat, lon);
+		} catch (SauronException e) {
+			if (e.getErrorMessage() == DUPLICATE_CAMERA)
+				System.out.println("Camera already registered. Processing observations...");
 
-				while (scanner.hasNextLine()) {
-					String line = scanner.nextLine();
-					if (!handledSpecialLine(cameraName, line, observations, info)) {
-						StringTokenizer st = new StringTokenizer(line, ",");
-						if (st.countTokens() != 2) {
-							info[1]++;
-							skipMessage("Invalid command.");
-							continue;
-						}
-						processObservation(observations, st, info);
-					}
-				}
-
-				int count_obs = observations.size();
-				if (count_obs > 0) {
-					submitObservations(cameraName, observations);
-					info[0] += count_obs;
-				}
-
-				scanner.close();
-				printReport(info);
-
-			} catch (Exception e) {
-				shutDownMessage("An unexpected error occurred.");
+			else {
+				shutDownMessage(e.getErrorMessageLabel());
+				return ;
 			}
 		}
-	}
 
-	private static void processObservation(List<List<String>> observations, StringTokenizer st, int[] info) {
-		String objectType = st.nextToken();
-		String objectId = st.nextToken();
+		try {
+			Scanner scanner = new Scanner(System.in);
+			List<List<String>> observations = new ArrayList<>();
 
-		if (checkObsArguments(objectType, objectId, info)) {
-			addObservation(observations, objectType, objectId);
+			while (scanner.hasNextLine()) {
+				String line = scanner.nextLine();
+				if (!handledSpecialLine(cameraName, line, observations)) {
+					StringTokenizer st = new StringTokenizer(line, ",");
+					if (st.countTokens() != 2) {
+						System.out.println("Invalid command.");
+						continue;
+					}
+					String objectType = st.nextToken();
+					String objectId = st.nextToken();
+
+					addObservation(observations, objectType, objectId);
+				}
+			}
+
+			if (!observations.isEmpty()) {
+				submitObservations(cameraName, observations);
+			}
+
+		} catch (Exception e) {
+			shutDownMessage("An unexpected error occurred.");
 		}
 	}
 
@@ -98,39 +96,11 @@ public class EyeApp {
 		observations.add(obs);
 	}
 
-	private static boolean registerCamera(String name, double lat, double lon) {
-		if (!name.matches("[A-Za-z0-9]+") || name.length() < 3 || name.length() > 15) {
-			shutDownMessage("Invalid camera name provided.");
-			return false;
-		}
-		else if (lat > 90 || lat < -90 || lon > 180 || lon < -180) {
-			shutDownMessage("Invalid coordinates provided.");
-			return false;
-		}
-
-		try {
-			_frontend.camJoin(name, lat, lon);
-			return true;
-		}
-		catch (SauronException e) {
-			if (e.getErrorMessage() == DUPLICATE_CAMERA) {
-				System.out.println("Camera already registered. Processing observations...");
-				return true;
-			}
-			else {
-				shutDownMessage(e.getErrorMessageLabel());
-				return false;
-			}
-		}
-	}
-
-	private static boolean handledSpecialLine(String camName, String line, List<List<String>> observations, int[] info) {
+	private static boolean handledSpecialLine(String camName, String line, List<List<String>> observations) {
 		// blank line
 		if (line.isBlank()) {
 			if (!observations.isEmpty()) {
-				int count_obs = observations.size();
 				submitObservations(camName, observations);
-				info[0] += count_obs;
 			}
 			return true;
 		}
@@ -142,8 +112,7 @@ public class EyeApp {
 		else if (line.startsWith("zzz")) {
 			StringTokenizer st = new StringTokenizer(line, ",");
 			if (st.countTokens() != 2) {
-				info[1]++;
-				skipMessage("Invalid command.");
+				System.out.println("Invalid command.");
 				return true;
 			}
 			st.nextToken();	// remove zzz
@@ -154,11 +123,11 @@ public class EyeApp {
 
 			}
 			catch (NumberFormatException ne) {
-				info[1]++;
-				skipMessage("Invalid command.");
+				System.out.println("Invalid command.");
+				return true;
 			}
 			catch (InterruptedException ie) {
-					Thread.currentThread().interrupt();
+				Thread.currentThread().interrupt();
 			}
 		}
 		// not a special line
@@ -172,76 +141,13 @@ public class EyeApp {
 		}
 		catch (SauronException e) {
 			System.out.println(e.getErrorMessageLabel());
-			shutDownMessage("Observation not submitted.");
+			observations.clear();
 		}
-
-	}
-
-	private static boolean checkObsArguments(String type, String id, int[] info) {
-		if (!type.equals(CAR)  && !type.equals(PERSON)) {
-			info[1]++;
-			skipMessage("Invalid object type provided.");
-			return false;
-		}
-		else if ((type.equals(CAR) && !checkCarId(id)) || (type.equals(PERSON) && !checkPersonId(id))) {
-			info[1]++;
-			skipMessage(String.format("Invalid %s identifier provided.", type));
-			return false;
-		}
-
-		return true;
-	}
-
-	private static boolean checkCarId(String id) {
-		int numFields = 0;
-
-		if (id.length() != 6)
-			return false;
-
-		for (int i = 0; i <3; i++){
-			char firstChar = id.charAt(2 * i);
-			char secChar = id.charAt(2 * i + 1);
-
-			if (Character.isDigit(firstChar) && Character.isDigit(secChar))
-				numFields++;
-
-			else if (!(Character.isUpperCase(firstChar) && Character.isUpperCase(secChar)))
-				return false;
-		}
-		if (numFields == 3 || numFields == 0){
-			return false;
-		}
-
-		return true;
-	}
-
-	private static boolean checkPersonId(String id) {
-		try{
-			Long.parseLong(id);
-			return Long.parseLong(id) > 0;
-		}
-		catch(NumberFormatException e){
-			return false;
-		}
-	}
-
-	private static void skipMessage(String msg) {
-		System.out.println(msg);
-		System.out.println("Skipping...");
 	}
 
 	private static void shutDownMessage(String msg) {
 		System.out.println(msg);
 		System.out.println("Shutting down...");
 	}
-
-	private static void printReport(int[] info) {
-		System.out.println("\n----------REPORT----------");
-		System.out.printf("Sent:      %d\n", info[0]);
-		System.out.printf("Skipped:   %d\n", info[1]);
-
-	}
-
-
 
 }
